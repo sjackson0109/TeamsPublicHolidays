713f322fe0d1ee5567ac679f9a2628b1071e4615
Simon Jackson <admin@jacksonfamily.me>
2024-03-15 12:37:49 +0000

    couple of adjustments

--- TeamsPublicHolidays.ps1
+++ TeamsPublicHolidays.ps1
@@ -23,79 +23,154 @@
     https://github.com/sjackson0109/TeamsScheduleNationalHolidays
     https://blog.jacksonfamily.me/Teams-ScheduleNationalHolidays
 #>
-
 function Get-PublicHolidays {
     Param(
-        [string]$CountryCode= "GB"
+        [string]$CountryCode = "GB",
+        [string]$Year = (Get-Date).Year
     )
-    $year = Get-Date -Format yyyy
-    $url = "https://date.nager.at/api/v3/PublicHolidays/$year/$CountryCode"
-    $holidaysResponse = Invoke-RestMethod -Uri $url -Method "GET"
-    $holidays = $holidaysResponse | ForEach-Object {
+    
+    $url = "https://date.nager.at/api/v3/PublicHolidays/$Year/$CountryCode"
+    Write-Debug "Fetching holidays from URL: $url"
+
+    try {
+        $holidaysResponse = Invoke-RestMethod -Uri $url -Method "GET"
+    } catch {
+        Write-Error "An error occurred while fetching holidays for country code ($CountryCode) and year ($year)"
+        Write-Host "TIP: Check you have supplied the correct CountryCode from the following website: https://www.iban.com/country-codes"
+        return @()
+    }
+    
+    Write-Debug "Number of unique holidays provided: $($holidaysResponse.Count)"
+
+    # Deduplicate holidays by date
+    $uniqueHolidays = $holidaysResponse | Select-Object -Unique Date, Name
+    
+    Write-Debug "Number of unique holidays filtered: $($uniqueHolidays.Count)"
+
+    #Filter only the columns necessary
+    $holidays = $uniqueHolidays | ForEach-Object {
         [PSCustomObject]@{
             Date = $_.date
             Name = $_.name
         }
     }
+
     return $holidays
 }
-function Update-TeamsPublicHolidays {
+
+function Create-TeamsPublicHolidays {
     Param(
-        [string]$CountryCode= "GB",
-        [string]$ScheduleName= "UK National Holidays"
+        [string]$CountryCode = "GB",
+        [string]$ScheduleName = "UK National Holidays",
+        [int]$Year = (Get-Date).Year
     )
-    $holidays = Get-PublicHolidays -CountryCode $CountryCode
-    $schedule = Get-CsOnlineSchedule | Where-Object { $_.Name -eq $ScheduleName }
-    Write-Host "UPDATING: $($schedule.Name)"
-    Write-Host "CURRENT DATES:"
-    $schedule.FixedSchedule.DateTimeRanges | Format-Table
-    $schedule.FixedSchedule.DateTimeRanges = @()
+
+    try {
+        # Get public holidays for the specified country code and year
+        $holidays = Get-PublicHolidays -CountryCode $CountryCode -Year $Year
+    } catch {
+        Write-Error "An error occurred while fetching holidays for country code ($CountryCode) and year ($Year)"
+        Write-Host "TIP: Check you have supplied the correct CountryCode from the following website: https://www.iban.com/country-codes"
+        return
+    }
+
+    # Define a date range some time in the past, so it won't be relevant
+    $initialDateTimeRange = New-CsOnlineDateTimeRange -Start "01/01/1900" -End "02/01/1900"
+    # Create a new CsOnlineSchedule for the specified schedule name
+    $schedule = New-CsOnlineSchedule -Name $ScheduleName -FixedSchedule -DateTimeRanges $initialDateTimeRange
+
+    # Add holidays to the schedule
     foreach ($holiday in $holidays) {
         $myDate = [datetime]::ParseExact($holiday.Date, 'yyyy-MM-dd', $null)
         $DateStart = $myDate.ToString('dd/MM/yyyy 00:00')
         $DateEnd = $myDate.AddDays(1).ToString('dd/MM/yyyy 00:00')
+
+        # Add the holiday to the schedule
         $schedule.FixedSchedule.DateTimeRanges += New-CsOnlineDateTimeRange -Start $DateStart -End $DateEnd
     }
-    Write-Host "NEW DATES:"
-    $schedule.FixedSchedule.DateTimeRanges | Format-Table
+
+    # Remove the initial date time range from the fixed schedule's date time ranges
+    $schedule.FixedSchedule.DateTimeRanges = $schedule.FixedSchedule.DateTimeRanges | Where-Object { $_ -ne $initialDateTimeRange }
+
+    # Output the dates added to the schedule
+    Write-Host "Dates added to schedule '$ScheduleName':"
+    $schedule.FixedSchedule.DateTimeRanges | Format-Table -Property Start, End -AutoSize
+
+    # Update the schedule in Microsoft Teams
     Set-CsOnlineSchedule -Instance $schedule | Out-Null
 }
 
-# function Create-TeamsPublicHolidays {
-#     Param(
-#         [string]$CountryCode= "FR",
-#         [string]$ScheduleName= "FR National Holidays"
-#     )
 
-#     # Check if a schedule with the provided name already exists
-#     $existingSchedule = Get-CsOnlineSchedule | Where-Object { $_.Name -eq $ScheduleName }
+function Update-TeamsPublicHolidays {
+    Param(
+        [string]$CountryCode = "GB",
+        [string]$ScheduleName = "UK National Holidays",
+        [int]$Year = (Get-Date).Year,
+        [switch]$Append=$false
+    )
+    
+    try {
+        # Get public holidays for the specified country code and year
+        $holidays = Get-PublicHolidays -CountryCode $CountryCode -Year $Year
+    } catch {
+        Write-Error "An error occurred while fetching holidays for country code ($CountryCode) and year ($Year)"
+        Write-Host "TIP: Check you have supplied the correct CountryCode from the following website: https://www.iban.com/country-codes"
+        return
+    }
 
-#     if ($existingSchedule) {
-#         Write-Host "EXISTING SCHEDULE: $($existingSchedule.Name)"
-#         $schedule = $existingSchedule
-#     } else {
-#         # Create an empty array for DateTimeRanges
-#         $dateTimeRanges = @()
+    # Retrieve existing schedule
+    $existingSchedule = Get-CsOnlineSchedule | Where-Object { $_.Name -eq $ScheduleName }
+    if ($existingSchedule -eq $null) {
+        Write-Error "No existing schedule found with name '$ScheduleName'"
+        return
+    }
 
-#         # Create a new schedule with FixedSchedule initialized with DateTimeRanges
-#         $schedule = New-CsOnlineSchedule -Name $ScheduleName -FixedSchedule @{DateTimeRanges = $dateTimeRanges}
+    # Output the current dates in the schedule
+    Write-Host "CURRENT DATES:"
+    $existingSchedule.FixedSchedule.DateTimeRanges | Format-Table -Property Start, End -AutoSize
 
-#         Write-Host "NEW SCHEDULE: $($schedule.Name)"
-#     }
+    if ($Append) {
+        foreach ($holiday in $holidays) {
+            $myDate = [datetime]::ParseExact($holiday.Date, 'yyyy-MM-dd', $null)
+            $DateStart = $myDate.ToString('dd/MM/yyyy 00:00')
+            $DateEnd = $myDate.AddDays(1).ToString('dd/MM/yyyy 00:00')
+            $dateTimeRange = New-CsOnlineDateTimeRange -Start $DateStart -End $DateEnd
+            $existingSchedule.FixedSchedule.DateTimeRanges += $dateTimeRange
+        }
+        # Output the new dates added to the schedule
+        Write-Host "NEW DATES:"
+        $existingSchedule.FixedSchedule.DateTimeRanges | Format-Table -Property Start, End -AutoSize
 
-#     # Attach public holidays to the schedule
-#     $holidays = Get-PublicHolidays -CountryCode $CountryCode
-#     foreach ($holiday in $holidays) {
-#         $myDate = [datetime]::ParseExact($holiday.Date, 'yyyy-MM-dd', $null)
-#         $DateStart = $myDate.ToString('dd/MM/yyyy 00:00')
-#         $DateEnd = $myDate.AddDays(1).ToString('dd/MM/yyyy 00:00')
-#         $schedule.FixedSchedule.DateTimeRanges += New-CsOnlineDateTimeRange -Start $DateStart -End $DateEnd
-#     }
+        # Update the schedule in Microsoft Teams
+        Set-CsOnlineSchedule -Instance $existingSchedule | Out-Null
+    } else {
+        # Define a date range some time in the past, so it won't be relevant
+        $initialDateTimeRange = New-CsOnlineDateTimeRange -Start "01/01/1900" -End "02/01/1900"
 
-#     # Output the updated schedule
-#     Write-Host "NEW DATES: $($schedule.Name)"
-#     $schedule.FixedSchedule.DateTimeRanges | Format-Table
+        # Create a new CsOnlineSchedule for the specified schedule name with the initial date time range
+        $schedule = New-CsOnlineSchedule -Name $ScheduleName -FixedSchedule -DateTimeRanges $initialDateTimeRange
 
-#     # Update the schedule
-#     Set-CsOnlineSchedule -Instance $schedule | Out-Null
-# }
+        # Process each holiday and add it to the schedule
+        foreach ($holiday in $holidays) {
+            $myDate = [datetime]::ParseExact($holiday.Date, 'yyyy-MM-dd', $null)
+            $DateStart = $myDate.ToString('dd/MM/yyyy 00:00')
+            $DateEnd = $myDate.AddDays(1).ToString('dd/MM/yyyy 00:00')
+
+            # Create a DateTimeRange for each holiday
+            $dateTimeRange = New-CsOnlineDateTimeRange -Start $DateStart -End $DateEnd
+
+            # Add the DateTimeRange to the schedule
+            $schedule.FixedSchedule.DateTimeRanges += $dateTimeRange
+        }
+
+        # Remove the initial date time range from the fixed schedule's date time ranges
+        $schedule.FixedSchedule.DateTimeRanges = $schedule.FixedSchedule.DateTimeRanges | Where-Object { $_ -ne $initialDateTimeRange }
+
+        # Output the dates added to the schedule
+        Write-Host "Dates added to schedule '$ScheduleName':"
+        $schedule.FixedSchedule.DateTimeRanges | Format-Table -Property Start, End -AutoSize
+
+        # Update the schedule in Microsoft Teams
+        Set-CsOnlineSchedule -Instance $schedule | Out-Null
+    }
+}
