13184835cd2c382c62f71b5b59c6b38145c231d0
Mitchell Bakker <30444264+mitchelljb@users.noreply.github.com>
2024-06-03 14:56:04 +0200

    Added suport for Regional holidays

--- Changelog.md
+++ Changelog.md
@@ -1,7 +1,8 @@
 # Changelog
 
-|Date|Component|Version|Change|
+|Date (dd/mm/yyyy)|Component|Version|Change|
 |---|---|---|---|
 |20.12.2023|Update-TeamsPublicHolidays.ps1||Creation with UK support only|
 |21.12.2023|Update-TeamsPublicHolidays.ps1||Updated to support Germany national holidays|
 |01.03.2024|Update-TeamsPublicHolidays.ps1||Published with new REST API endpoint|
+|04.06.2024|Update-TeamsPublicHolidays.ps1||Added support for Regional filtering |
--- TeamsPublicHolidays.ps1
+++ TeamsPublicHolidays.ps1
@@ -7,17 +7,24 @@
 
 .EXAMPLE
     Update-TeamsPublicHolidays -ScheduleName 'UK National Holidays' -CountryCode 'GB'
-    #This command updates the 'UK National Holidays' schedule in Microsoft Teams with the public holidays for the United Kingdom.
+    #This command updates the 'UK National Holidays' schedule in Microsoft Teams with the public holidays for the United Kingdom. Default behaviour is append, but can be overuled with the "-NotAppend" switch. 
+	It will only add the future holidays, all past holidays will not be added. 
 
     Create-TeamsPublicHolidays -ScheduleName 'FR National Holidays' -CountryCode 'FR'
-    # This command creates a new schedule named 'FR National Holidays' in Microsoft Teams and attaches the public holidays for France.
+    # This command creates a new schedule named 'FR National Holidays' in Microsoft Teams and attaches the public holidays for France. 
+	It will only add the future holidays, all past holidays will not be added. 
+	
+	Create-TeamsPublicHolidays -ScheduleName 'DE Regional Bavaria Holidays' -CountryCode 'DE' -Region 'BY'
+    # This command creates a new schedule named 'DE Regional Bavaria Holidays' in Microsoft Teams and attaches the National public holidays for Germany including the Regional holidays for the Region (Bavaria). 
+	It will only add the future holidays, all past holidays will not be added. 
 
 
 .NOTES
-    File Name      : Update-TeamsPublicHolidays.ps1
-    Author         : Simon Jackson (@sjackson0109)
-    Prerequisite   : PowerShell 3.0 or above, Microsoft Teams module installed
-    Last Modified  : 2024/03/01
+    File Name      	: Update-TeamsPublicHolidays.ps1
+    Author         	: Simon Jackson (@sjackson0109)
+	Updated by		: Mitchell Bakker (mitchelljb)
+    Prerequisite   	: PowerShell 3.0 or above, Microsoft Teams module installed
+    Last Modified  	: 2024/06/03
 
 .LINK
     https://github.com/sjackson0109/TeamsScheduleNationalHolidays
@@ -25,7 +32,8 @@
 #>
 function Get-PublicHolidays {
     Param(
-        [string]$CountryCode = "GB",
+        [string]$CountryCode = "NL",
+		[string]$Region = "",
         [string]$Year = (Get-Date).Year
     )
     
@@ -43,7 +51,7 @@ function Get-PublicHolidays {
     Write-Debug "Number of unique holidays provided: $($holidaysResponse.Count)"
 
     # Deduplicate holidays by date
-    $uniqueHolidays = $holidaysResponse | Select-Object -Unique Date, Name
+    $uniqueHolidays = $holidaysResponse | Select-Object -Unique Date, Name, Global, Counties
     
     Write-Debug "Number of unique holidays filtered: $($uniqueHolidays.Count)"
 
@@ -52,10 +60,15 @@ function Get-PublicHolidays {
         [PSCustomObject]@{
             Date = $_.date
             Name = $_.name
+			Global = $_.global
+			Region = if($_.counties){ $_.counties | foreach {$_.split("-")[1]} }
         }
     }
-
-    return $holidays
+    if ($region -ne "") {
+			return $holidays | where {$_.region -contains $region -or $_.global -eq $True} 
+	}
+	else {return $holidays | where {$_.global -eq $True}
+	}
 }
 
 function Create-TeamsPublicHolidays {
@@ -77,20 +90,21 @@ function Create-TeamsPublicHolidays {
     # Define a date range some time in the past, so it won't be relevant
     $initialDateTimeRange = New-CsOnlineDateTimeRange -Start "01/01/1900" -End "02/01/1900"
     # Create a new CsOnlineSchedule for the specified schedule name
-    $schedule = New-CsOnlineSchedule -Name $ScheduleName -FixedSchedule -DateTimeRanges $initialDateTimeRange
+    $schedule = New-CsOnlineSchedule -Name "$ScheduleName" -FixedSchedule -DateTimeRanges $initialDateTimeRange
 
     # Add holidays to the schedule
     foreach ($holiday in $holidays) {
         $myDate = [datetime]::ParseExact($holiday.Date, 'yyyy-MM-dd', $null)
         $DateStart = $myDate.ToString('dd/MM/yyyy 00:00')
         $DateEnd = $myDate.AddDays(1).ToString('dd/MM/yyyy 00:00')
-
+		
         # Add the holiday to the schedule
         $schedule.FixedSchedule.DateTimeRanges += New-CsOnlineDateTimeRange -Start $DateStart -End $DateEnd
     }
 
     # Remove the initial date time range from the fixed schedule's date time ranges
-    $schedule.FixedSchedule.DateTimeRanges = $schedule.FixedSchedule.DateTimeRanges | Where-Object { $_ -ne $initialDateTimeRange }
+	# Remove all hisatorical events
+    $schedule.FixedSchedule.DateTimeRanges = $schedule.FixedSchedule.DateTimeRanges | Where-Object { $_.end -ge (Get-date) }
 
     # Output the dates added to the schedule
     Write-Host "Dates added to schedule '$ScheduleName':"
@@ -106,7 +120,7 @@ function Update-TeamsPublicHolidays {
         [string]$CountryCode = "GB",
         [string]$ScheduleName = "UK National Holidays",
         [int]$Year = (Get-Date).Year,
-        [switch]$Append=$false
+        [switch]$NotAppend=$false
     )
     
     try {
@@ -119,7 +133,7 @@ function Update-TeamsPublicHolidays {
     }
 
     # Retrieve existing schedule
-    $existingSchedule = Get-CsOnlineSchedule | Where-Object { $_.Name -eq $ScheduleName }
+    $existingSchedule = Get-CsOnlineSchedule | Where-Object { $_.Name -eq "$ScheduleName" }
     if ($existingSchedule -eq $null) {
         Write-Error "No existing schedule found with name '$ScheduleName'"
         return
@@ -129,14 +143,17 @@ function Update-TeamsPublicHolidays {
     Write-Host "CURRENT DATES:"
     $existingSchedule.FixedSchedule.DateTimeRanges | Format-Table -Property Start, End -AutoSize
 
-    if ($Append) {
+    if (-not $NotAppend) {
         foreach ($holiday in $holidays) {
             $myDate = [datetime]::ParseExact($holiday.Date, 'yyyy-MM-dd', $null)
             $DateStart = $myDate.ToString('dd/MM/yyyy 00:00')
             $DateEnd = $myDate.AddDays(1).ToString('dd/MM/yyyy 00:00')
-            $dateTimeRange = New-CsOnlineDateTimeRange -Start $DateStart -End $DateEnd
-            $existingSchedule.FixedSchedule.DateTimeRanges += $dateTimeRange
+			if ($existingSchedule.FixedSchedule.DateTimeRanges.Start -notcontains $holiday.date){
+				$dateTimeRange = New-CsOnlineDateTimeRange -Start $DateStart -End $DateEnd
+				$existingSchedule.FixedSchedule.DateTimeRanges += $dateTimeRange 
+			}
         }
+		$existingSchedule.FixedSchedule.DateTimeRanges = $existingSchedule.FixedSchedule.DateTimeRanges | Where-Object { $_.end -ge (Get-date) }
         # Output the new dates added to the schedule
         Write-Host "NEW DATES:"
         $existingSchedule.FixedSchedule.DateTimeRanges | Format-Table -Property Start, End -AutoSize
@@ -148,24 +165,23 @@ function Update-TeamsPublicHolidays {
         $initialDateTimeRange = New-CsOnlineDateTimeRange -Start "01/01/1900" -End "02/01/1900"
 
         # Create a new CsOnlineSchedule for the specified schedule name with the initial date time range
-        $schedule = New-CsOnlineSchedule -Name $ScheduleName -FixedSchedule -DateTimeRanges $initialDateTimeRange
+        $schedule = New-CsOnlineSchedule -Name "$ScheduleName" -FixedSchedule -DateTimeRanges $initialDateTimeRange
 
         # Process each holiday and add it to the schedule
         foreach ($holiday in $holidays) {
             $myDate = [datetime]::ParseExact($holiday.Date, 'yyyy-MM-dd', $null)
             $DateStart = $myDate.ToString('dd/MM/yyyy 00:00')
             $DateEnd = $myDate.AddDays(1).ToString('dd/MM/yyyy 00:00')
-
+				
             # Create a DateTimeRange for each holiday
             $dateTimeRange = New-CsOnlineDateTimeRange -Start $DateStart -End $DateEnd
 
             # Add the DateTimeRange to the schedule
-            $schedule.FixedSchedule.DateTimeRanges += $dateTimeRange
+            $schedule.FixedSchedule.DateTimeRanges += $dateTimeRange 
         }
 
         # Remove the initial date time range from the fixed schedule's date time ranges
-        $schedule.FixedSchedule.DateTimeRanges = $schedule.FixedSchedule.DateTimeRanges | Where-Object { $_ -ne $initialDateTimeRange }
-
+		$schedule.FixedSchedule.DateTimeRanges = $schedule.FixedSchedule.DateTimeRanges | Where-Object { $_.end -ge (Get-date) }
         # Output the dates added to the schedule
         Write-Host "Dates added to schedule '$ScheduleName':"
         $schedule.FixedSchedule.DateTimeRanges | Format-Table -Property Start, End -AutoSize
